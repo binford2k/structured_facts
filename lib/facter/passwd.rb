1eef8c2786b8f1fc59501a4f900d1f19ae575326
Zack Smith <zack@puppetlabs.com>
2013-07-22 13:28:17 +0100

    Fix whitespace

--- lib/facter/passwd.rb
+++ lib/facter/passwd.rb
@@ -5,7 +5,7 @@ Facter.add(:encoded_passwd) do
   setcode do
     passwd = {}
     Etc.passwd do |user|
-    passwd[user.name] = Hash[user.each_pair.to_a]
+      passwd[user.name] = Hash[user.each_pair.to_a]
     end
     passwd.to_json
   end
