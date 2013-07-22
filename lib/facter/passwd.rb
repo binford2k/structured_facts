956697c315ce66bba4c64f5a41c60057ad37fa1e
Zack Smith <zack@puppetlabs.com>
2013-07-22 13:21:29 +0100

    Add passwd fact

---
+++ lib/facter/passwd.rb
@@ -0,0 +1,12 @@
+require 'json'
+require 'etc'
+Facter.add(:encoded_passwd) do
+  confine :kernel => "Linux"
+  setcode do
+    passwd = {}
+    Etc.passwd do |user|
+    passwd[user.name] = Hash[user.each_pair.to_a]
+    end
+    passwd.to_json
+  end
+end
